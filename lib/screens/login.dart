680d5b3d43d3d8c8989fe156841465d07600f148
anishkha02 <anishkha0208@gmail.com>
2022-11-09 00:12:56 +0530

    fifth day

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -16,48 +16,51 @@ class LoginPage extends StatelessWidget {
       //     )
       // ),
 
-      child: Column(
-        children: [
-          Image.asset("assets/images/login.png",
-          fit: BoxFit.cover,),
-          SizedBox(height: 20,),   // Used for spacing and good presentation of the interface..//
-          Text("Welcome",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 24
-          ),),
-          SizedBox(height: 20,),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0,horizontal: 32.0),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: "Enter username",
-                    labelText: "UserName"
+      child: SingleChildScrollView( //this is used to adjust the page according to various devices vo scrolling krdega rather than showing overflow//
+        child: Column(
+          children: [
+            Image.asset("assets/images/login.png",
+              //height: 500,//
+            fit: BoxFit.cover,),
+            SizedBox(height: 20,),   // Used for spacing and good presentation of the interface..//
+            Text("Welcome",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 24
+            ),),
+            SizedBox(height: 20,),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0,horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Enter username",
+                      labelText: "UserName"
 
+                    ),
                   ),
-                ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      hintText: "Enter password",
-                      labelText: "Password",
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        hintText: "Enter password",
+                        labelText: "Password",
 
+                    ),
                   ),
-                ),
-                SizedBox(height: 20,),
-                ElevatedButton(
-                  child: Text("Login"),
-                    style: TextButton.styleFrom(),
-                  onPressed: (){
-                    print("Hi my name is Anishkha...");
-                  },
-                )
-              ],
-            ),
-          )
+                  SizedBox(height: 20,),
+                  ElevatedButton(
+                    child: Text("Login"),
+                      style: TextButton.styleFrom(),
+                    onPressed: (){
+                      print("Hi my name is Anishkha...");
+                    },
+                  )
+                ],
+              ),
+            )
 
-          
-        ],
+            
+          ],
 
+        ),
       ),
     );
   }
